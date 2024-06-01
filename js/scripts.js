33c039583d2db9b6a57cb2abde19d6f09527a9aa
donije09 <ustinedon@gmail.com>
2024-06-01 14:07:11 +0200

    pokomon Anfang
    Add intial pokomon datei

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
-alert('hello world');
-var favoriteFood = 'dodo';
-document.write('Favorite food: ' + favoriteFood);
+let pokomonList = [
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'dauda',
+        height: 9,
+        types: ['water', 'rat']
+    },
+    {
+        namm: "jaho",
+        height: 4,
+        type: ['suger', 'light']
 
 
+    }
+
+];
