1e5215b92564b363741b28ec37f8954f85766dd6
donije09 <ustinedon@gmail.com>
2024-06-09 18:45:23 +0200

    update part 2
    Ex. no. 1.5
    pokenon

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,38 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 7,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'dauda',
-        height: 9,
-        types: ['water', 'rat']
-    },
-    {
-        name: "jaho",
-        height: 4,
-        type: ['sugar', 'light']
-
-
-    }
-
-];
- 
-pokemonList.forEach(function(pokemon) {
+let pokemonRepository = (function(){
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 7,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'dauda',
+            height: 9,
+            types: ['water', 'rat']
+        },
+        {
+            name: "jaho",
+            height: 4,
+            type: ['sugar', 'light']
+    
+    
+        }
+    
+    ];
+    return{
+        getAll: function() {
+            return pokemonList;
+        },
+        add: function(pokemon){
+            if (typeof pokemon === 'object' && 'name' in pokemon && 'height' in pokemon && 'types' in pokemon){
+                pokemonList.push(pokemon);
+            } else {
+                console.log('Invalid Pokemon object');
+            }
+        }
+    };
+})();    
+pokemonRepository.getAll().forEach(function(pokemon){
     let output = pokemon.name + '(height:' + pokemon.height + ')';
 
     if (pokemon.height > 6.0) {
